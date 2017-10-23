d7da382b46dddfc340200d39c700b5f79743e8ce
Nicolas Baptiste <nicolas.baptiste@iadvize.com>
2017-10-23 15:09:48 +0200

    more tests

---
+++ myTransforms/__testfixtures__/factory/factoryRequireReact.output.js
@@ -0,0 +1,34 @@
+/**
+ * We should keep this comment
+ */
+'use strict';
+import React from 'react';
+
+export default function() {
+
+  const CLASSNAMES = {
+    container: 'actionsList'
+  };
+
+  /**
+   * Actions list
+   * This component is used to display actions
+   */
+  return React.createClass({
+    displayName: 'ActionsList',
+
+    statics: {
+      CLASSNAMES
+    },
+
+    render() {
+      const { children } = this.props;
+
+      return (
+        <div className={CLASSNAMES.container}>
+          {children}
+        </div>
+      );
+    }
+  });
+};
---
+++ myTransforms/__testfixtures__/factory/factoryWithoutUseStrict.input.js
@@ -0,0 +1,32 @@
+/**
+ * We should keep this comment
+ */
+
+module.exports = function (React) {
+
+  const CLASSNAMES = {
+    container: 'actionsList'
+  };
+
+  /**
+   * Actions list
+   * This component is used to display actions
+   */
+  return React.createClass({
+    displayName: 'ActionsList',
+
+    statics: {
+      CLASSNAMES
+    },
+
+    render() {
+      const { children } = this.props;
+
+      return (
+        <div className={CLASSNAMES.container}>
+          {children}
+        </div>
+      );
+    }
+  });
+};
